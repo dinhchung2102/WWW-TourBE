a866c25a94ebb20dc0761881a2ff2a02dbf7d904
dinhchung2102 <vodinhchung4@gmail.com>
2025-12-07 13:07:22 +0700

    update

---
+++ src/main/java/fit/se/tourbe/TourBeApplication.java
@@ -0,0 +1,13 @@
+package fit.se.tourbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fit/se/tourbe/TourBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se.tourbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
