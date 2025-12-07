d88e2928a71b6c08876a81a5a01bcfe7d301fafa
dinhchung2102 <vodinhchung4@gmail.com>
2025-12-07 18:46:09 +0700

    update: cor config

--- src/main/java/fit/se/tourbe/config/SecurityConfig.java
+++ src/main/java/fit/se/tourbe/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://nagentech.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token", "*"));
         configuration.setAllowCredentials(true);
